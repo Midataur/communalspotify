049800df8f0740350e9b3acd417a613dfe6227c9
Darcy LF <ehnevault@gmail.com>
2020-11-30 09:49:07 +1100

    refactor(components): move Header.js to DefaultHeader.js to avoid confusion

--- components/DefaultHeader.js
+++ components/DefaultHeader.js
@@ -1,6 +1,6 @@
 // Requires: Link
 
-function Header(){
+function DefaultHeader(){
 	return (
 		html`
 			<div class="flex justify-between border-b-2 py-3 items-center">
@@ -9,12 +9,15 @@ function Header(){
 				</h1>
 				
 				<div class="flex ">
+					<${Slot} name="headerNav">
 						<div class="mr-3">
 							<${Link} href="/join" text="Join Room" />
 						</div>
 						<div class="">
 							<${Link} href="/create" text="Create Room" />
 						</div>
+					<//>
+						
 				</div>
 			</div>
 		`
