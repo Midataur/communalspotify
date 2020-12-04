82ee37f45afc9a97d0852b971eba9e7927fd60e9
Darcy LF <ehnevault@gmail.com>
2020-12-04 11:18:38 +1100

    feat(components): add close button to serch ui

--- components/Search.js
+++ components/Search.js
@@ -49,16 +49,23 @@ function Search() {
     return (
         html`
         <div class="h-full w-full fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-50 z-50 p-8 overflow-scroll">
-            <div class="bg-white text-black m-8 p-8 base mx-auto w-full rounded-lg shadow-lg overflow-scroll">
-            <form onSubmit=${onSubmit} action="#" class="mb-4">
-                <input type="search" name="search" class="form-input block w-full mt-1 rounded text-black text-xl" value=${value} onInput=${onInput} placeholder="Search for a song" autoFocus/>
-                <button type="submit" class="mt-2 bg-page-bg w-full py-2 text-page-contrast rounded">Search</button>
-            </form>
-            <div>
-                Results: please ignore bad ui
-                
-                ${searchResults ? Options() : "empty"}
-            </div>
+            <div class="base mx-auto m-8">
+              <div class="bg-white text-black p-8 py-10  w-full rounded-lg shadow-lg overflow-scroll relative">
+                  <div class="top-0 right-0 absolute">
+                      <button onClick=${closeSearchUI} class="p-1 mt-2 m-1 text-gray-600 hover:text-gray-800 leading-none"><i class='bx bxs-x-circle'></i></button>
+                      
+                  </div>
+                  <form onSubmit=${onSubmit} action="#" class="mb-4">
+                      <input type="search" name="search" class="form-input block w-full mt-1 rounded text-black text-xl" value=${value} onInput=${onInput} placeholder="Search for a song" autoFocus/>
+                      <button type="submit" class="mt-2 bg-page-bg w-full py-2 text-page-contrast rounded">Search</button>
+                  </form>
+                  <div>
+                      Results: please ignore bad ui
+                      
+                      ${searchResults ? Options() : "empty"}
+                  </div>
+              </div>
+    
             </div>
         </div>
         
